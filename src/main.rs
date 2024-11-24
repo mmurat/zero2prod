87a30691a15278e4410bb7c72fce9f3627ef58c1
mmurat <arica@protonmail.com>
2024-11-24 06:56:49 +0300

    24.11.2024 6:56

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run()?.await
 }
+
+/* use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check() ->  HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+
+        HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+
+} 
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds()  {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
+}
+
+ */
